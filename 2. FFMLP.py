ea4e1c971babc6f04a93c9cdd7dade7b5cf9f168
jameslindesay <jameslindesay2@gmail.com>
2022-03-13 22:26:24 +1100

    forward train and loss

--- 2. FFMLP.py
+++ 2. FFMLP.py
@@ -26,8 +26,13 @@ def truncated_normal(mean=0, sd=1, low=0, upp=10):
     return truncnorm((low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
 
 # Sigmoid activation
-def activate(x):
-    return 1/(1 + np.exp(-x))
+# def activate(x):
+#     return 1/(1 + np.exp(-x))
+
+# softmax
+def activate(vector):
+    e = np.exp(vector)
+    return e / e.sum()
 
 # Leaky ReLu Activation
 # def activate(x):
@@ -37,9 +42,9 @@ def activate(x):
 #         return 0.01*x
 
 def initialise_weights(input_size, h1_size, h2_size, output_size):
-    input_h1_weights = truncated_normal(mean=2, sd=1, low=-1/np.sqrt(input_size), upp=1/np.sqrt(input_size)).rvs((h1_size, input_size))
-    h1_h2_weights = truncated_normal(mean=2, sd=1, low=-1/np.sqrt(h1_size), upp=1/np.sqrt(h1_size)).rvs((h2_size, h1_size))
-    h2_output_weights = truncated_normal(mean=2, sd=1, low=-1/np.sqrt(h2_size), upp=1/np.sqrt(h2_size)).rvs((output_size, h2_size))
+    input_h1_weights = truncated_normal(mean=0, sd=1, low=-1/np.sqrt(input_size), upp=1/np.sqrt(input_size)).rvs((h1_size, input_size))
+    h1_h2_weights = truncated_normal(mean=0, sd=1, low=-1/np.sqrt(h1_size), upp=1/np.sqrt(h1_size)).rvs((h2_size, h1_size))
+    h2_output_weights = truncated_normal(mean=0, sd=1, low=-1/np.sqrt(h2_size), upp=1/np.sqrt(h2_size)).rvs((output_size, h2_size))
     return(input_h1_weights, h1_h2_weights, h2_output_weights)
 
 def initialise_biases(h1_size, h2_size, output_size):
@@ -58,16 +63,32 @@ def forward_propagate(input_data, input_h1_weights, h1_h2_weights, h2_output_wei
     output_vector = activate(np.matmul(h2_output_weights, h2_vector) + output_biases)
     return input_vector, h1_vector, h2_vector, output_vector
 
-# TODO: run batch of training set, calculate loss, backpropagate, run on test
+def classify(input_data, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases):
+    a, b, c, output_vector = forward_propagate(input_data, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases)
+    return np.where(output_vector == np.amax(output_vector))[0][0], np.amax(output_vector)
+
+def forward_train_batch(input_indices, df, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases):
+    classifications = []
+    for index in input_indices:
+        input_data = mnist_row_to_input_vector(df, index)
+        classification, probability = classify(input_data, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases)
+        classifications.append((index, classification, df.iloc[index]['label'], probability))    
+    return classifications
+
+def calculate_batch_loss(classifications):
+    loss = 0
+    for classification in classifications:
+        loss -= (classification[1] == classification[2]) * np.log(classification[3])
+    return loss
+
+# TODO: backpropagate, run on test
 
 def main():
     df_train, df_test = read_data()
     h1_biases, h2_biases, output_biases = initialise_biases(h1_size, h2_size, output_size)
     input_h1_weights, h1_h2_weights, h2_output_weights = initialise_weights(input_size, h1_size, h2_size, output_size)
-    input_data = mnist_row_to_input_vector(df_train, 0)
-    input_vector, h1_vector, h2_vector, output_vector = forward_propagate(input_data, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases)
-    print(input_vector.shape)
-    print(output_vector)
+    classifications = forward_train_batch([i for i in range(10)], df_train, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases)
+    loss = calculate_batch_loss(classifications)
 
 if __name__ == '__main__':
     main()
