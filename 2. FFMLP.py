3ec218b4ce93614383aa0ab5571e7b048b426dcf
jameslindesay <jameslindesay2@gmail.com>
2022-03-14 01:12:25 +1100

    Fixed some loss, still confused about bp

--- 2. FFMLP.py
+++ 2. FFMLP.py
@@ -17,6 +17,7 @@ input_size = 784
 h1_size = 392
 h2_size = 196
 output_size = 10
+learning_rate = 0.01
 
 def read_data():
     return pd.read_csv('fashion_data/fashion-mnist_train.csv'), pd.read_csv('fashion_data/fashion-mnist_test.csv')
@@ -30,16 +31,26 @@ def truncated_normal(mean=0, sd=1, low=0, upp=10):
 #     return 1/(1 + np.exp(-x))
 
 # softmax
-def activate(vector):
+def softmax(vector):
     e = np.exp(vector)
     return e / e.sum()
 
 # Leaky ReLu Activation
-# def activate(x):
-#     if x>0 :
-#         return x
-#     else :
-#         return 0.01*x
+def ReLU(x):
+    if x > 0:
+        return x
+    else:
+        return 0.01 * x
+# vectorise for use with arrays
+ReLU_v = np.vectorize(ReLU)
+
+def ReLU_deriv(x):
+    if x > 0:
+        return 1
+    else:
+        return 0.01
+# vectorise for use with arrays
+ReLU_deriv_v = np.vectorize(ReLU_deriv)
 
 def initialise_weights(input_size, h1_size, h2_size, output_size):
     input_h1_weights = truncated_normal(mean=0, sd=1, low=-1/np.sqrt(input_size), upp=1/np.sqrt(input_size)).rvs((h1_size, input_size))
@@ -57,29 +68,40 @@ def mnist_row_to_input_vector(df, row):
     return np.array(df.iloc[row].values.tolist()[1:], ndmin=2).T
 
 def forward_propagate(input_data, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases):
-    input_vector = activate(input_data)
-    h1_vector = activate(np.matmul(input_h1_weights, input_vector) + h1_biases)
-    h2_vector = activate(np.matmul(h1_h2_weights, h1_vector) + h2_biases)
-    output_vector = activate(np.matmul(h2_output_weights, h2_vector) + output_biases)
+    input_vector = input_data
+    h1_vector = ReLU_v(np.matmul(input_h1_weights, input_vector) + h1_biases)
+    h2_vector = ReLU_v(np.matmul(h1_h2_weights, h1_vector) + h2_biases)
+    output_vector = softmax(np.matmul(h2_output_weights, h2_vector) + output_biases)
     return input_vector, h1_vector, h2_vector, output_vector
 
-def classify(input_data, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases):
+# Returns classification, classified probability, target probability
+def classify(input_data, df, input_index, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases):
     a, b, c, output_vector = forward_propagate(input_data, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases)
-    return np.where(output_vector == np.amax(output_vector))[0][0], np.amax(output_vector)
+    return np.where(output_vector == np.amax(output_vector))[0][0], np.amax(output_vector), output_vector[df.iloc[input_index]['label']][0]
 
-def forward_train_batch(input_indices, df, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases):
+def classification_loss(target_probability):
+    return -(np.log(target_probability))
+
+def forward_propagate_batch(input_indices, df, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases):
     classifications = []
     for index in input_indices:
         input_data = mnist_row_to_input_vector(df, index)
-        classification, probability = classify(input_data, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases)
-        classifications.append((index, classification, df.iloc[index]['label'], probability))    
+        classification, classification_probability, target_probability = classify(input_data, df, index, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases)
+        error = classification_loss(target_probability)
+        classifications.append((index, classification, df.iloc[index]['label'], classification_probability, target_probability, error))    
     return classifications
 
-def calculate_batch_loss(classifications):
-    loss = 0
-    for classification in classifications:
-        loss -= (classification[1] == classification[2]) * np.log(classification[3])
-    return loss
+# def calculate_batch_loss(classifications):
+#     loss = 0
+#     for classification in classifications:
+#         loss -= (classification[1] == classification[2]) * np.log(classification[3])
+#     return loss
+
+### BACKPROPAGATION: https://doug919.github.io/notes-on-backpropagation-with-cross-entropy/
+
+# def backpropagate_batch(input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases, learning_rate):
+#     input_h1_weights_updated = -learning_rate * 
+#     return input_h1_weights_updated, h1_h2_weights_updated, h2_output_weights_updated, h1_biases_updated, h2_biases_updated, output_biases_updated
 
 # TODO: backpropagate, run on test
 
@@ -87,8 +109,8 @@ def main():
     df_train, df_test = read_data()
     h1_biases, h2_biases, output_biases = initialise_biases(h1_size, h2_size, output_size)
     input_h1_weights, h1_h2_weights, h2_output_weights = initialise_weights(input_size, h1_size, h2_size, output_size)
-    classifications = forward_train_batch([i for i in range(10)], df_train, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases)
-    loss = calculate_batch_loss(classifications)
+    classifications = forward_propagate_batch([i for i in range(10)], df_train, input_h1_weights, h1_h2_weights, h2_output_weights, h1_biases, h2_biases, output_biases)
+    print(classifications)
 
 if __name__ == '__main__':
     main()
